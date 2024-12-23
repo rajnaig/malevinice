93c102248d0e9ac95cb6bf2344c4faeacbd06b90
Gábor Rajnai <gabika.hun@gmail.com>
2024-12-23 14:24:10 +0100

    refactor: remove unused Footer import from page component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 // app/page.tsx
 import { Feature } from "@/components/feature"
-import Footer from "@/components/footer"
 import { Header } from "@/components/header"
 import { Hero } from "@/components/hero"
 import WineShowcase from "@/components/showcase"
